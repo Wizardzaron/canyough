f895edaa8e0367b32a0bc0ea0af7a05d28e7da10
Fynn <Tung02nguyen10@gmail.com>
2023-08-24 11:52:16 -0700

    FEAT: assembly without using assembly PR
    im bad at jokes

---
+++ canPR.py
@@ -0,0 +1,16 @@
+#hello 
+unsigned int add(unsigned int m, unsigned int n) {
+  goto c;
+  a:
+    int i=0;
+  b:
+    if (n = 0) return m;
+    if m = succ(m);
+    goto d;
+  c:
+    goto a;
+  d:
+  i++;
+  n--;
+  goto b;
+}
